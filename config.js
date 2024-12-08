99ccc45967c03aa1e002d3a8ebb7e966b096fd6f
Jaswant Yadav <jaswantyadav742@gmail.com>
2024-12-08 09:56:54 +0530

    first commit

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connectdb = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database:'schools'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password:process.env.DB_PASSWORD || '',
+    database:process.env.DB_NAME || 'schools'
 });
 
 connectdb.connect((err) => {
